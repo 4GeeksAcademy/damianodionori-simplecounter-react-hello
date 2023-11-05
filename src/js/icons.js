2c75865d6d80b5a6c6b9afa53e5c1a4ad9b4713b
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-05 22:25:34 +0000

    simple counter

---
+++ src/js/icons.js
@@ -0,0 +1,9 @@
+import fontawesome from '@fortawesome/fontawesome';
+import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+fontawesome.config = {
+    autoReplaceSvg: 'nest'
+}
+fontawesome.library.add(faClock);
+
+import "./icons.js";
+<i className="far fa-clock"></i>
