b92b9ef6ef119bce77f63f9ad1192b349061f5e5
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-06 11:25:18 +0000

    adjusted index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,35 +8,43 @@ import "../styles/index.css";
 import propTypes from "prop-types";
 
 function SimpleCounter(props) {
-    return (
+  return (
     <div className="bigCounter">
-        <div className="calendar">
-            <i className="far fa-clock"></i>
-        </div>
-        <div className="four">{props.digitFour % 10}</div>
-        <div className="three">{props.digitThree % 10}</div>
-        <div className="two">{props.digitTwo % 10}</div>
-        <div className="one">{props.digitOne % 10}</div>
+      <div className="calendar">
+        <i className="far fa-clock"></i>
+      </div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
     </div>
-    );
-};
+  );
+}
 
 SimpleCounter.protoTypes = {
-    digitFour: propTypes.number,
-    digitThree: propTypes.number,
-    digitTwo: propTypes.number,
-    digitOne: propTypes.number,
+  digitFour: propTypes.number,
+  digitThree: propTypes.number,
+  digitTwo: propTypes.number,
+  digitOne: propTypes.number,
 };
 
 const counter = 0;
 
-setInterval(function() {
-    const four = Math.floor(counter/1000);
-    const three = Math.floor(counter/100);
-    const two = Math.floor(counter/10);
-    const one = Math.floor(counter/1);
-    console.log(four, three, two, one);
-    counter++;
-    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, document.querySelector("#app"));
+setInterval(function () {
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor(counter / 100);
+  const two = Math.floor(counter / 10);
+  const one = Math.floor(counter / 1);
+  console.log(four, three, two, one);
+  counter++;
+  ReactDOM.render(
+    <SimpleCounter
+      digitOne={one}
+      digitTwo={two}
+      digitThree={three}
+      digitFour={four}
+    />,
+    document.querySelector("#app")
+  );
 }, 1000);
 //render your react application
